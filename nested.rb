49f83a383713bc4b22751bfff2a0308efb817d5e
Filip Todoroski <ftodoroski@ymail.com>
2019-11-16 00:42:33 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -104,7 +104,7 @@ def changing_alan
           :languages => ["COBOL", "FORTRAN"]
         },
         :alan_kay => {
-          :known_for => "Object Orientation",
+          alans_new_info = "GUI",
           :languages => ["Smalltalk", "LISP"]
         },
         :dennis_ritchie => {
