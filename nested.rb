c3d6d3155098e4959e94a47b74749c1660f167a0
Filip Todoroski <ftodoroski@ymail.com>
2019-11-16 00:40:00 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -75,10 +75,10 @@ def adding_matz
 
 	programmer_hash =
  		{
-			:yukihiro_matsumoto => {
-			  :known_for => "Ruby",
-			   :languages => ["LISP", "C"]
-			},
+			# :yukihiro_matsumoto => {
+			#   :known_for => "Ruby",
+			#    :languages => ["LISP", "C"]
+			# },
         :grace_hopper => {
           :known_for => "COBOL",
           :languages => ["COBOL", "FORTRAN"]
@@ -93,7 +93,7 @@ def adding_matz
         }
      }
 
-		programmer_hash[:yukihiro_matsumoto]
+		# programmer_hash[:yukihiro_matsumoto]
 end
 
 def changing_alan
